a8bef7726272f50dd13aa3945ac710ddd2ed9e32
Parker Kemp <parker.kemp@hitcents.com>
2014-07-18 17:00:23 -0500

    Deny players from using claimed horses.

--- src/com/spinalcraft/nachohorse/NachoHorse.java
+++ src/com/spinalcraft/nachohorse/NachoHorse.java
@@ -1,7 +1,5 @@
 package com.spinalcraft.nachohorse;
 
-import java.util.Hashtable;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -14,33 +12,11 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTameEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.spinalcraft.spinalpack.Co;
 import com.spinalcraft.spinalpack.Spinalpack;
 
-class PlayerFlags{
-	private static Hashtable<String, PlayerFlags> playerFlags = new Hashtable<String, PlayerFlags>();
-	public boolean willClaim;
-	
-	public PlayerFlags(){
-		willClaim = false;
-	}
-	
-	public static void add(Player player){
-		playerFlags.put(player.getName(), new PlayerFlags());
-	}
-	
-	public static void remove(Player player){
-		playerFlags.remove(player.getName());
-	}
-	
-	public static PlayerFlags fromPlayer(Player player){
-		return playerFlags.get(player.getName());
-	}
-}
 
 public class NachoHorse extends JavaPlugin implements Listener{
 
@@ -56,27 +32,26 @@ public class NachoHorse extends JavaPlugin implements Listener{
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if(cmd.getName().equalsIgnoreCase("claimhorse")){
-			if(sender instanceof Player){
-				Player player = (Player)sender;
-				PlayerFlags.fromPlayer(player).willClaim = true;
-			}
-			return true;
-		}
 		return false;
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerInteractEntity(PlayerInteractEntityEvent event){
-		//if(!(event.getRightClicked() instanceof Horse))
-		//	return;
-		//if(!PlayerFlags.fromPlayer(event.getPlayer()).willClaim)
-		//	return;
-		//console.sendMessage("Clicked horse.");
+		if(!(event.getRightClicked() instanceof Horse))
+			return;
 		
-		//Player player = event.getPlayer();
+		Horse horse = (Horse)event.getRightClicked();
+		String ownerName = Spinalpack.petOwner(horse.getUniqueId());
 		
-		//PlayerFlags.fromPlayer(player).willClaim = false;
+		if(ownerName == null)
+			return;
+		
+		Player player = event.getPlayer();
+		
+		if(!player.getName().equals(ownerName)){
+			event.setCancelled(true);
+			player.sendMessage(Spinalpack.code(Co.RED) + "That's not your horse!");
+		}
 	}
 	
 	@EventHandler(priority = EventPriority.NORMAL)
@@ -88,18 +63,6 @@ public class NachoHorse extends JavaPlugin implements Listener{
 		Horse horse = (Horse)event.getEntity();
 		Player player = (Player)event.getOwner();
 		Spinalpack.insertPet(player, horse);
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void onPlayerJoin(PlayerJoinEvent event){
-		Player player = event.getPlayer();
-		PlayerFlags.add(player);
-	}
-	
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void onPlayerQuit(PlayerQuitEvent event){
-		Player player = event.getPlayer();
-		PlayerFlags.remove(player);
 	}
 	
 	@Override
